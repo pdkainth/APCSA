6114b4008a9a48d73376fd56989ad4adaa9292c6
pdkainth <33015564+pdkainth@users.noreply.github.com>
2019-01-07 11:21:49 -0800

    Add files via upload

---
+++ CheckingAccountDriver.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author Your name
+ * Period #
+ */
+public class CheckingAccountDriver {
+	public static void main(String[] args)
+	{
+		// Your code goes here
+		
+		try {
+			CheckingAccount acc1 = new CheckingAccount(-345, 100);
+		} catch (ArithmeticException e){
+			System.out.println(e.getMessage());
+			
+			try {
+				CheckingAccount acc2 = new CheckingAccount(345, 102);
+				acc2.deposit(-33);
+			} catch (ArithmeticException e2) {
+				System.out.println(e2.getMessage());
+				
+				try {
+					CheckingAccount acc3 = new CheckingAccount(322, 103);
+					acc3.withdraw(345);
+				} catch (ArithmeticException e3) {
+					System.out.println(e3.getMessage());
+				}
+			}
+		}
+	}
+}
