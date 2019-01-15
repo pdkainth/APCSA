85b57f5e4bb6e8611dc3aa72df9cccbe4280b27a
Prabhdeep Kainth <pdkainth@gmail.com>
2019-01-15 10:55:22 -0800

    Rolling lab

---
+++ LabA14/src/Rolling.java
@@ -0,0 +1,65 @@
+import java.util.*;
+/**
+ *   
+ * @author Prabhdeep Kainth
+ * period # 4
+ */
+  
+public class Rolling {
+	Random rand;
+	/**
+	 * constructor to initilize the random number
+	 * generator
+	 * @param s - seed for the random number generator
+	 */
+	public Rolling(int s)
+	{
+		rand = new Random(s);//makes new number generator
+	}
+	/**
+	 * Method to simulate a dice roll
+	 * @return an integer between 1 and 6
+	 */
+    private int roll() 
+    { 
+        //Your code goes here
+    	return rand.nextInt(5) + 1;
+    }
+    
+    /**  
+     * Roll the dice three times and count the
+     * number of tries it took to get all three
+     * different rolls. In the end print a message
+     * displaying the number of tries
+     */
+    public  void play() {
+        //Your code goes here
+    	int count = 1;
+    	boolean same = true;
+    	
+    	while(same) {
+    		int first = roll();
+    		int second = roll();
+    		int third = roll();
+    		
+    		System.out.println(first + " " + second + " " + third);
+    		
+    		if(first != second && second != third && first != third) {
+    			System.out.println("count = " + count);
+    			same = false;
+    		} else {
+    			count++;
+    		}
+    		
+    		
+    	}
+    	
+    	
+    }
+
+
+    /** 
+     * Answer to question 3 goes here
+     */
+  
+} 
